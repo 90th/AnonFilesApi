04ba807dc6bf7cb7784e8eee2cb737a9035911e1
z <80871202+90th@users.noreply.github.com>
2023-07-22 00:55:43 -0400

    1.0.0

---
+++ AnonFilesApiClient.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+public class AnonfilesApiClient
+{
+    private const string BaseUrl = "https://api.anonfiles.com";
+
+    private readonly HttpClient httpClient;
+
+    public AnonfilesApiClient()
+    {
+        httpClient = new HttpClient();
+    }
+
+    public async Task<UploadResponse> UploadFileAsync(string filePath)
+    {
+        using (var formData = new MultipartFormDataContent())
+        {
+            formData.Add(new StreamContent(new FileStream(filePath, FileMode.Open)), "file", Path.GetFileName(filePath));
+            var response = await httpClient.PostAsync($"{BaseUrl}/upload", formData);
+            var responseBody = await response.Content.ReadAsStringAsync();
+            return Newtonsoft.Json.JsonConvert.DeserializeObject<UploadResponse>(responseBody);
+        }
+    }
+
+    public async Task<FileInfoResponse> GetFileInfoAsync(string fileId)
+    {
+        var response = await httpClient.GetAsync($"{BaseUrl}/v2/file/{fileId}/info");
+        var responseBody = await response.Content.ReadAsStringAsync();
+        return Newtonsoft.Json.JsonConvert.DeserializeObject<FileInfoResponse>(responseBody);
+    }
+}
+
+public class UploadResponse
+{
+    public bool Status { get; set; }
+    public UploadResponseData Data { get; set; }
+    public Error Error { get; set; } // Add this line to include the Error property
+}
+
+public class UploadResponseData
+{
+    public UploadedFile File { get; set; }
+}
+
+public class UploadedFile
+{
+    public FileUrl Url { get; set; }
+    public FileMetadata Metadata { get; set; }
+}
+
+public class FileUrl
+{
+    public string Full { get; set; }
+    public string Short { get; set; }
+}
+
+public class FileMetadata
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public FileSize Size { get; set; }
+}
+
+public class FileSize
+{
+    public int Bytes { get; set; }
+    public string Readable { get; set; }
+}
+
+public class FileInfoResponse
+{
+    public bool Status { get; set; }
+    public FileInfoResponseData Data { get; set; }
+    public Error Error { get; set; } // Add this line to include the Error property
+}
+
+public class FileInfoResponseData
+{
+    public UploadedFile File { get; set; }
+}
+
+public class Error
+{
+    public string Message { get; set; }
+    public string Type { get; set; }
+    public int Code { get; set; }
+}
+
---
+++ Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace AnonFilesAPI
+{
+    internal class Program
+    {
+        static async Task Main()
+        {
+            try
+            {
+                AnonfilesApiClient anonfilesApiClient = new AnonfilesApiClient();
+
+                Console.WriteLine("Choose an option:");
+                Console.WriteLine("1. Upload a file");
+                Console.WriteLine("2. Retrieve file information by file ID");
+                Console.Write("Enter your choice (1 or 2): ");
+                string choice = Console.ReadLine();
+
+                if (choice == "1")
+                {
+                    Console.Write("Enter the file path to upload: ");
+                    string filePath = Console.ReadLine();
+
+                    if (!File.Exists(filePath))
+                    {
+                        Console.WriteLine("File not found: " + filePath);
+                        return;
+                    }
+
+                    UploadResponse uploadResponse = await anonfilesApiClient.UploadFileAsync(filePath);
+
+                    if (uploadResponse.Status)
+                    {
+                        // File uploaded successfully
+                        var uploadedFile = uploadResponse.Data.File;
+
+                        Console.WriteLine("File URL: " + uploadedFile.Url.Full);
+                        Console.WriteLine("File ID: " + uploadedFile.Metadata.Id);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        // Handle the error response
+                        Console.WriteLine("Error: " + uploadResponse.Error.Message);
+                    }
+                }
+                else if (choice == "2")
+                {
+                    Console.Write("Enter the file ID to retrieve file information: ");
+                    string fileId = Console.ReadLine();
+
+                    FileInfoResponse fileInfoResponse = await anonfilesApiClient.GetFileInfoAsync(fileId);
+
+                    if (fileInfoResponse.Status)
+                    {
+                        // File info retrieved successfully
+                        var file = fileInfoResponse.Data.File;
+                        Console.WriteLine("File URL: " + file.Url.Full);
+                        Console.WriteLine("File Name: " + file.Metadata.Name);
+                        Console.WriteLine("File Size: " + file.Metadata.Size.Readable);
+                        Console.ReadKey();
+                    }
+                    else
+                    {
+                        // Handle the error response
+                        Console.WriteLine("Error: " + fileInfoResponse.Error.Message);
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please choose option 1 or 2.");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
+
+
+    }
+}
